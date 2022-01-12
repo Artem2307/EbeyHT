0892726b307379acb86ad427f5fa5ff6fdb7b78a
Artem2307 <Pravdyk1@gmail.com>
2022-01-12 15:47:53 +0200

    Test

--- src/test/java/Test/BuySmart.java
+++ src/test/java/Test/BuySmart.java
@@ -20,5 +20,9 @@ public class BuySmart {
         driver.findElement(By.xpath("//img[@alt=\"UNLOCKED Samsung Galaxy Note 9 SM-N960U 128GB BLACK N960U Smartphone Black Color\"]")).click();
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//a[@href=\"https://www.ebay.com/atc/binctr?item=174673681209&rev=11&fromPage=2047675&fb=1&gch=1\"]")));
     }
+    public void buySmartphoneCard(){
+        driver.findElement(By.id("isCartBtn_btn")).click();
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//button[@class=\"call-to-action btn btn--large btn--primary\"]")));
+    }
 
 }
